33786e92166bcd2e3f1be3cfd97c4099d9fd3620
xzuoqi <xzq@radxa.com>
2024-09-05 11:48:46 +0800

    feat: customised navigation bar

---
+++ src/theme/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Navbar from '@theme-original/Navbar';
+import NewNavBar from "@site/src/components/CustomNav";
+
+export default function NavbarWrapper(props) {
+  return (
+    <>
+      {/* <Navbar {...props} /> */}
+      <NewNavBar />
+    </>
+  );
+}
