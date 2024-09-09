53895bd3704b18719b28f4f4247958f6c4e7aca6
xzuoqi <xzq@radxa.com>
2024-09-09 09:20:44 +0800

    fix: mobile navigation bar display problems

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import Navbar from '@theme-original/Navbar';
-import NewNavBar from "@site/src/components/CustomNav";
+import Navbar from '@theme-original/Navbar';
+// import NewNavBar from "@site/src/components/CustomNav";
 
 export default function NavbarWrapper(props) {
   return (
     <>
-      {/* <Navbar {...props} /> */}
-      <NewNavBar />
+      <Navbar {...props} />
+      {/* <NewNavBar /> */}
     </>
   );
 }
